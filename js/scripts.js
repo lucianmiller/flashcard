6a7156325c00ff881fa4da666852dc3e1c39de79
Lucian <lucian.miller185@gmail.com>
2021-03-15 10:49:45 -0700

    It will add toggle to naming conventions card

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,10 @@ $(document).ready(function() {
     $("#varibles-showing").toggle();
     $("#varibles-hidden").toggle();
   });
+
+  $(".clickable3").click(function() {
+    $("#naming-showing").toggle();
+    $("#naming-hidden").toggle();
+  });
 });
 
